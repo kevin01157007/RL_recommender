9fea21e81ac39d1bf21278aa8bbd18b88cd05d69
kevin01157007 <152252977+kevin01157007@users.noreply.github.com>
2025-05-07 20:11:46 +0800

    embedding數量調整(測試)

--- data_split/split.py
+++ data_split/split.py
@@ -4,15 +4,15 @@ import pandas as pd
 ratings = pd.read_csv('../raw/ml-1m/ratings.dat', sep='::', names=['user_id', 'movie_id', 'rating', 'timestamp'], engine='python', encoding='latin-1')
 movies  = pd.read_csv('../raw/ml-1m/movies.dat', sep='::', names=['movie_id', 'title', 'genres'],   engine='python', encoding='latin-1')
 users   = pd.read_csv('../raw/ml-1m/users.dat',  sep='::', names=['user_id', 'gender', 'age', 'occupation', 'zip'], engine='python', encoding='latin-1')
-
 # 先過濾出評分大於 4 的評分記錄
-ratings_above_4 = ratings[ratings['rating'] > 4]
-
+ratings = ratings[ratings['rating'] > 3]
+# print(len(ratings.user_id.unique()))
+# print(len(ratings.movie_id.unique()))
 # 計算每個使用者的評分數量
-user_rating_counts = ratings_above_4['user_id'].value_counts()
+user_rating_counts = ratings['user_id'].value_counts()
 
 # 獲取評分數量大於等於 10 的使用者 ID
-valid_users = user_rating_counts[user_rating_counts > 10].index
+valid_users = user_rating_counts[user_rating_counts > 9].index
 
 # 使用這些有效的使用者 ID 來過濾原始的評分數據
 ratings_filtered = ratings[ratings['user_id'].isin(valid_users)].copy()
@@ -22,5 +22,5 @@ test_df        = inter_df.loc[test_idx].reset_index(drop=True)
 test_df.to_csv("test.dat", sep=',', index=False)
 train_df   = ratings_filtered.drop(test_idx).reset_index(drop=True)
 train_df.to_csv("train.dat", sep=',', index=False)
-# print(test_df.head())
-# print(train_df.head())
+print(test_df.head())
+print(train_df.head())
